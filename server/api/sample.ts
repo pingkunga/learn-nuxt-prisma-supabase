12695659a9992a5f450c4d4f5daf45f10f5c5fb8
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-11-16 15:28:13 +0700

    feat: create error sample

--- server/api/sample.ts
+++ server/api/sample.ts
@@ -1,4 +1,13 @@
 export default defineEventHandler((event) => {
+
+    //เข้า middleware ก่อน
+    if (event.context.auth.role !== 'admin') {
+        throw createError({
+            status: 401,
+            message: 'Unauthorized'
+        });
+    }
+
     return {
         status: 200,
         body: {
