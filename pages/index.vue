88598cc06bdd701e98a4e6fc346832f8810c2ca8
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-11-17 16:37:43 +0700

    feat: init + test daisy ui

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,10 @@
         <h1 class="text-3xl font-bold underline">
             Hello world!
         </h1>
+
+        <button class="btn btn-primary">
+            daisyui button
+        </button>
     </div>
 </template>
 
