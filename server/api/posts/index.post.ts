08924c55f15015c03691cfc7178dc9e3ea27eade
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-11-17 09:17:52 +0700

    feat: add get api

---
+++ server/api/posts/index.post.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+
+type Tuser = {
+    name: string;
+    email: string;
+    posts?: Tpost[];
+}
+
+type Tpost = {
+    title: string;
+    content: string;
+    published: boolean;
+    author: Tuser;
+}
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+    const reqBody = await readBody<Tpost>(event);
+    console.log(reqBody);
+    if (!reqBody.author) {
+        throw new Error("Author is missing in the request body");
+    }
+    
+    const post = await prisma.post.create({
+        data: {
+            title: reqBody.title,
+            content: reqBody.content,
+            published: reqBody.published,
+            author: {
+                //connectOrCreate ถ้าไม่มีให้สร้างใหม่เลย
+                //!. คือ ไม่ให้มีค่า null
+                connectOrCreate: {
+                    where: { 
+                        email: reqBody.author!.email 
+                    },
+                    create: { 
+                        name: reqBody.author!.name, 
+                        email: reqBody.author!.email 
+                    }
+                }
+            }
+        }
+    });
+
+    return post;
+});
